b0b0f516843363e5f4823a886d47ee2071d335b7
heeinso <heeinso@snu.ac.kr>
2018-09-03 17:05:02 +0900

    Add some actions

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import { FETCH_ALL_BOOKS, CHANGE_BOOK_SHELF } from '../constants/actionType';
+
+export const fetchAllBooks = () => {
+  return {
+    type: FETCH_ALL_BOOKS,
+  };
+};
+
+export const changeBookShelf = (book, shelf) => {
+  return {
+    type: CHANGE_BOOK_SHELF,
+    payload: {
+      book,
+      shelf,
+    },
+  };
+};
