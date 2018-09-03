a73d1d62a28b1d4ad97c4a5ed1524729c1455637
heeinso <heeinso@snu.ac.kr>
2018-09-03 23:02:40 +0900

    Modify Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 
+import { NONE } from '../constants/shelfType';
 import BookShelfChanger from './BookShelfChanger';
-
-const BookCover = styled.div`
-  width: 128px;
-  height: 192px;
-  background-image: ${props =>
-    props.thumbnail ? `url(${props.thumbnail})` : ''};
-`;
 
 class Book extends Component {
   render() {
     const { book, shelf } = this.props;
+    let thumbnail = undefined;
+    if (book.imageLinks) {
+      thumbnail = book.imageLinks.thumbnail;
+    }
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <BookCover
+            <div
               className="book-cover"
-              thumbnail={book.imageLinks.thumbnail}
+              style={{
+                width: 128,
+                height: 192,
+                backgroundImage: `url(${thumbnail})`,
+              }}
             />
-            <BookShelfChanger book={book} shelf={shelf} />
+            <BookShelfChanger book={book} shelf={shelf || NONE} />
           </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">{book.authors}</div>
